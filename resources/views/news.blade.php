5c93ebb1d6f433e05e2495c8623b6cb0658587c3
Sergey Misharin <misharin_sergey-work@mail.ru>
2023-04-20 00:25:26 +0300

    lesson-1

---
+++ resources/views/news.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+</head>
+<nav>
+    <ul>
+        <li><a href="/">Главная</a></li>
+        <li><a href="/about">О проекте</a></li>
+        <li><a href="/news">Новости</a></li>
+
+    </ul>
+</nav>
+<body>
+<h1>News</h1>
+<p>Это страница с новостями.</p>
+</body>
+</html>
